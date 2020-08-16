20f12bde9495ff197ed560cd1f33b1a4d26eb4fe
SarthakBhargava <46133998+SarthakBhargava@users.noreply.github.com>
2020-08-16 08:15:52 +0530

    Add files via upload

---
+++ web/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Subjects)
+admin.site.register(Topics)
+admin.site.register(Ebooks)
+admin.site.register(Certificate)
